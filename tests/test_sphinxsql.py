6268dc547d9fc2a77658ce3f6f3b270758b6847f
Marcus Robb <marcus.robb@initworx.com>
2021-07-08 10:49:41 -0700

    Cleaned up to remove flake8 errors.

--- tests/test_sphinxsql.py
+++ tests/test_sphinxsql.py
@@ -1,12 +1,13 @@
 import pytest
 from sphinx.application import Sphinx
 
+
 @pytest.fixture
-def sphinx_project(tempdir):
-    srcdir = tmpdir.mkdir('src')
-    outdir = tmpdir.mkdir('out')
-    add_file(srcdir, 'conf.py', '''
-    extensions = [ 'sphinx_sql.sphinx_sql' ]
-    ''')
-    yield (srcdir, outdir)
+def sphinx_project(self, tmpdir):
+    src_dir = tmpdir.mkdir('src')
+    out_dir = tmpdir.mkdir('out')
+    # add_file(src_dir, 'conf.py', '''
+    # extensions = [ 'sphinx_sql.sphinx_sql' ]
+    # ''')
+    yield src_dir, out_dir
  
